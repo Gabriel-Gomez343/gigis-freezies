7516c8f51f76a096d3bc3aa7545e92104c707cf9
DavidSAngel <salazar.davidangel@gmail.com>
2025-03-04 00:27:14 -0800

    Update Menu.js
    Adding the list of Flavors for the Italian Ice

--- src/Menu.js
+++ src/Menu.js
@@ -22,9 +22,32 @@ function Menu() {
     >
       <h2>Menu</h2>
 
-      <ImageCarousel images={files}
+      <h2>Italian Ice</h2>
+      <p>Lemon</p>
+      <p>Key Lime</p>
+      <p>Cherry</p>
+      <p>Black Cherry</p>
+      <p>Blueberry</p>
+      <p>Raspberry</p>
+      <p>Strawberry</p>
+      <p>Orange</p>
+      <p>Grape</p>
+      <p>Mango</p>
+      <p>Peach</p>
+      <p>Passion Fruit</p>
+      <p>Pina Coloda</p>
+      <p>Pineapple</p>
+      <p>Watermelon</p>
+      <p>Cantelop</p>
+      <p>Honey Dew</p>
+      <p>Sour Apple</p>
+      <p>Cotton Candy</p>
+      <p>Bubble Gum</p>
+      
+
+      {/*<ImageCarousel images={files}
           
-           />
+           />*/}
       
     </div>
   );
